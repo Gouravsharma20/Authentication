98cbd6d77af49d48f5c2a24c7b79e3557996eccd
Gouravsharma20 <gouravsharma20a@gmail.com>
2024-07-28 19:44:18 +0530

    app is not running but overall code is fine

--- app/src/main/java/wu/tutorials/firebase/MainActivity.kt
+++ app/src/main/java/wu/tutorials/firebase/MainActivity.kt
@@ -1,47 +1,102 @@
 package wu.tutorials.firebase
 
+import android.content.IntentSender
+import android.database.CursorJoiner
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.auth.api.identity.Identity
+import kotlinx.coroutines.launch
+import wu.tutorials.firebase.Presentation.Sign_in.GoogleAuthUiClient
+import wu.tutorials.firebase.Presentation.Sign_in.SignInScreen
+import wu.tutorials.firebase.Presentation.Sign_in.SignInViewModel
 import wu.tutorials.firebase.ui.theme.FireBaseTheme
 
 class MainActivity : ComponentActivity() {
+    private val googleAuthUiClient by lazy {
+        GoogleAuthUiClient(
+            context = applicationContext ,
+            oneTapClient = Identity.getSignInClient(applicationContext)
+        )
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FireBaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "Sign-in") {
+                        composable("Sign_in") {
+                            val viewModel = viewModel<SignInViewModel>()
+                            val state by viewModel.state.collectAsStateWithLifecycle()
+
+                            val launcher = rememberLauncherForActivityResult (
+                                contract = ActivityResultContracts.StartIntentSenderForResult(),
+                                onResult = { result ->
+                                    if(result.resultCode == RESULT_OK) {
+                                        lifecycleScope.launch {
+                                            val signInResult = googleAuthUiClient.signInWithIntent(
+                                                intent = result.data ?:return@launch
+                                            )
+                                            viewModel.onSignInResult(signInResult)
+                                        }
+                                    }
+                                }
+                            )
+
+                            SignInScreen(state = state, onSignInClick = {
+                                lifecycleScope.launch {
+                                    val signInIntentSender = googleAuthUiClient.signIn()
+                                    launcher.launch(
+                                        IntentSenderRequest.Builder(signInIntentSender?:return@launch
+                                        ).build()
+                                    )
+                                }
+                            }
+                            )
+                            LaunchedEffect(key1 = state.isSignInSuccessful) {
+                                if(state.isSignInSuccessful) {
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "sign in successful",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                                
+                            }
+                        }
+                    }
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FireBaseTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/wu/tutorials/firebase/Presentation/Sign_in/SignInState.kt
+++ app/src/main/java/wu/tutorials/firebase/Presentation/Sign_in/SignInState.kt
@@ -1,3 +1,6 @@
 package wu.tutorials.firebase.Presentation.Sign_in
 
-data class SignInState()
+data class SignInState(
+    val isSignInSuccessful : Boolean = false,
+    val signInError : String? = null
+)
